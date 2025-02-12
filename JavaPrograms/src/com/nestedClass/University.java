64387393e1f1c3bddf3e820e0ea05fca4663b7a0
CodeForgeNet <codeforgenet@icloud.com>
2025-02-12 19:01:00 +0530

    Initial commit

--- JavaPrograms/src/com/nestedClass/University.java
+++ JavaPrograms/src/com/nestedClass/University.java
@@ -1,9 +1,29 @@
 package com.nestedClass;
 
-import java.util.Scanner;
-
 public class University {
-	  
-	  
-	
+
+	static class Department {
+
+		String deptName;
+		int facultyNo;
+
+		public Department(String deptName, int facultyNo) {
+			this.deptName = deptName;
+			this.facultyNo = facultyNo;
+		}
+
+		public void displayInfo() {
+			System.out.println("Department : " + deptName);
+			System.out.println("Total faculty : " + facultyNo);
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		University.Department dept = new University.Department("CS", 50);
+		dept.displayInfo();
+
+	}
+
 }
