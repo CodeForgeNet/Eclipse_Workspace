14fed87b72be14bd94801730e8eb71df3d018f2f
CodeForgeNet <codeforgenet@icloud.com>
2025-07-22 12:01:43 +0530

    update

--- StriversDSA/src/hashing/OccurrenceByMap.java
+++ StriversDSA/src/hashing/OccurrenceByMap.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-
 public class OccurrenceByMap {
 
 //Occurrence of a number in an Array using Map.
@@ -22,37 +21,34 @@ public class OccurrenceByMap {
 			System.out.println(entry.getKey() + " - " + entry.getValue());
 		}
 	}
-	
-	
+
 //	Occurrence of a character in String using Map.
 	public static void countCharFreq(String str, int sn) {
 		Map<Character, Integer> map = new HashMap<Character, Integer>();
-		for(int i=0 ; i<sn; i++) {
-			if(map.containsKey(str.charAt(i))) {
-				map.put(str.charAt(i), map.get(str.charAt(i))+1);
-			}else {
+		for (int i = 0; i < sn; i++) {
+			if (map.containsKey(str.charAt(i))) {
+				map.put(str.charAt(i), map.get(str.charAt(i)) + 1);
+			} else {
 				map.put(str.charAt(i), 1);
 			}
 		}
-		//Traverse through map
-		for(Map.Entry<Character, Integer> entry : map.entrySet()) {
-			System.out.println(entry.getKey()+" - "+entry.getValue());
+		// Traverse through map
+		for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+			System.out.println(entry.getKey() + " - " + entry.getValue());
 		}
 	}
-	
-	
 
 	public static void main(String[] args) {
 		int[] arr = { 10, 5, 10, 15, 10, 5 };
 		int n = arr.length;
 		countFreq(arr, n);
-		
+
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter String : ");
 		String str = sc.next();
 		int sn = str.length();
-		countCharFreq(str,sn);
-		
+		countCharFreq(str, sn);
+
 		sc.close();
 	}
 }
